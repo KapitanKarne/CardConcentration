a4c511a27dd770c9567ab5640524400ffc7cfda2
Jazer Rebong <jazer.rebong@gmail.com>
2019-06-18 21:46:24 -0400

    Initial Commit

---
+++ CardConcentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardConcentration
+//
+//  Created by Ralph Jazer Rebong on 6/18/19.
+//  Copyright © 2019 Ralph Jazer Rebong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
